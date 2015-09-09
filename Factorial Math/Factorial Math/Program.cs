9a7a75cb138f17f02f9e5f0d7b7cd0e1b1054998
Michael B. <mbilenko03@gmail.com>
2015-09-08 20:00:18 -0500

    Added My Programs

---
+++ Factorial Math/Factorial Math/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialPractice
+{
+  class FactorialPracticeProgram
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter a number:");
+      string imFactorial = Console.ReadLine();
+      int valFactorial = int.Parse(imFactorial);
+      Stopwatch sw = new Stopwatch();
+      sw.Start();
+      double printFactorial = FactorialOpperation(valFactorial);
+      sw.Stop();
+      Console.WriteLine(printFactorial.ToString("#"));
+      Console.WriteLine("Time Elapsed is " + sw.Elapsed);
+    }
+
+    static double FactorialOpperation(int numFactorial)
+    {
+      double ansFactorial = 1;
+      for (int i = 2; i <= numFactorial; i++)
+      {
+        ansFactorial = ansFactorial * i;
+      }
+      return ansFactorial;
+    }
+
+  }
+}
---
+++ Integer Tester Math/Integer Tester Math/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+  class IntegerTester
+  {
+
+    #region Intiger Tester
+
+    static void Main(string[] args)
+    {
+      double number;
+      do
+      {
+        Console.Write("Type in a number: ");
+        string userNumber = Console.ReadLine();
+
+
+        bool result = double.TryParse(userNumber, out number);
+        if (result == true)
+        {
+          if (number % 2 == 0 && number != 0)
+          {
+            Console.WriteLine(number + " is even.");
+          }
+          else if (number % 2 == 1)
+          {
+            Console.WriteLine(number + " is odd.");
+          }
+          else if (number == 0)
+          {
+            Console.WriteLine(number + " ends the program.");
+            return;
+          }
+          else
+          {
+            Console.WriteLine(number + " is not an integer");
+          }
+
+        }
+        else
+        {
+          Console.WriteLine("It is not a number... You can't fool us!");
+        }
+      }
+      while (true);
+    }
+    #endregion
+  }
+}
