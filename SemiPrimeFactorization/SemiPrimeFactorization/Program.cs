d344f9a9b877ecfd13b4518cc3d8ba07384972c5
Michael B. <mbilenko03@gmail.com>
2015-09-09 20:36:52 -0500

    PreReleases on SemiPrimes || In Progress

---
+++ SemiPrimeFactorization/SemiPrimeFactorization/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemiPrimeFactorization
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter what term you want: ");
+      int number;
+      bool result = (int.TryParse(Console.ReadLine(), out number));
+      if (result == true)
+      {
+        //improgress
+      }
+      else
+      {
+        Console.WriteLine("Your Imput {0} is Invalid", number);
+      }
+      
+    }
+  }
+}
